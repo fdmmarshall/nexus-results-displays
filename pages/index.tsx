7d6c29ff5c99b3ef4a239a80707493f918ad9933
Flor Marshall <flordmarshall@gmail.com>
2022-07-20 16:34:30 -0400

    type changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,41 +2,38 @@ import type { NextPage } from "next";
 import fetchQuery from "../lib/query";
 import { useEffect, useState } from "react";
 import {
-  ColumnDef,
+  createColumnHelper,
   flexRender,
   getCoreRowModel,
   useReactTable,
 } from "@tanstack/react-table";
 import styles from "../styles/Home.module.css";
 
-type DinosaurType = {
-  types: string;
-  count: number;
+type Dinosaur = {
+  _id: number;
+  dinosaurName: string;
+  englishTranslation: string;
+  period: object;
+  dinoType: object;
+  taxonomy: object;
+  link: string;
 };
 
-const columns: ColumnDef<DinosaurType>[] = [
-  {
-    accessorKey: "types",
-    header: () => "Types",
-    cell: (info) => info.getValue(),
-    footer: (info) => info.column.id,
-  },
-  {
-    accessorKey: "count",
-    header: () => "Count",
-    cell: (info) => info.getValue(),
-    footer: (info) => info.column.id,
-  },
-];
+const columnHelper = createColumnHelper<Dinosaur>();
+
+const columns: Dinosaur[] = [];
 
 const Home: NextPage = () => {
-  const [data, setData] = useState<DinosaurType[]>([]);
+  const [data, setData] = useState<Dinosaur[]>([]);
 
   useEffect(() => {
     const queryResults = async () => {
-      const data = await fetchQuery();
+      const results = await fetchQuery();
+      console.log({ results });
 
-      setData(data);
+      if (results.isArray() && results.length > 0) {
+        setData(results);
+      }
     };
 
     queryResults();
@@ -51,8 +48,6 @@ const Home: NextPage = () => {
   return (
     <div className={styles.container}>
       <div className="container mx-auto sm:px-6 lg:px-8">
-        {/* Content goes here */}
-
         <table>
           <thead>
             {table.getHeaderGroups().map((headerGroup) => (
@@ -70,6 +65,17 @@ const Home: NextPage = () => {
               </tr>
             ))}
           </thead>
+          <tbody>
+            {/* {table.getRowModel().rows.map((row) => (
+              <tr key={row.id}>
+                {row.getVisibleCells().map((cell) => (
+                  <td key={cell.id}>
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </td>
+                ))}
+              </tr>
+            ))} */}
+          </tbody>
         </table>
       </div>
     </div>
