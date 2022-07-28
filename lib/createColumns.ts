50d0c8668498313ffe4fe72ba457e1a8ec2bb90e
Flor Marshall <flordmarshall@gmail.com>
2022-07-28 12:56:01 -0400

    clean up predicates

--- lib/createColumns.ts
+++ lib/createColumns.ts
@@ -1,8 +1,11 @@
-import { Predicates } from "../types/props";
+import { Predicate } from "../types/props";
 
-export default function createColumns(predicates: Predicates[]) {
+export default function createColumns(predicates: Predicate[]) {
 
+    const filteredPredicates = predicates.map(predicate => [predicate.name.substring(predicate.name.indexOf("/") + 1), predicate.type])
 
+    console.log(filteredPredicates)
 
+    return filteredPredicates
 
 }
